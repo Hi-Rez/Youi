4e6165d222c12ef6fe4922f1bc0ee75385829087
rezaali <syed.reza.ali@gmail.com>
2020-05-20 15:37:48 -0700

    added patch to hide Inspector when the app is deactivated

--- Youi/macOS/InspectorWindow.swift
+++ Youi/macOS/InspectorWindow.swift
@@ -11,19 +11,19 @@ import Cocoa
 open class InspectorWindow: NSWindow {
     public var inspectorViewController: InspectorViewController?
 
-    public init() {
+    public init(_ title: String = "") {
         super.init(contentRect: NSMakeRect(500, 500, 240, 240), styleMask: [.closable, .miniaturizable, .resizable, .fullSizeContentView, .titled], backing: .buffered, defer: false)
-        setup()
+        setup(title)
     }
 
-    open func setup() {
+    open func setup(_ title: String) {
         titlebarAppearsTransparent = true
-        title = "Inspector"
-        setFrameAutosaveName("Inspector")
+        self.title = title.isEmpty ? "Inspector" : title
+        setFrameAutosaveName(self.title)
         backgroundColor = .clear
         isReleasedWhenClosed = false
         level = .statusBar
-        hidesOnDeactivate = false
+        hidesOnDeactivate = true
         setupInspector()
         contentView = inspectorViewController?.view
     }
