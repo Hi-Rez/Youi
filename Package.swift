6c8689c9afca8be2a35eb19d3d51483c1b37d6b6
Reza Ali <syed.reza.ali@gmail.com>
2022-02-18 10:56:26 -0800

    set Youi satin dependency to 1.6.1

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "Youi",
     platforms: [.macOS(.v10_15), .iOS(.v14)],
     products: [.library(name: "Youi", targets: ["Youi"])],
-    dependencies: [.package(url: "https://github.com/Hi-Rez/Satin", .branch("master"))],
+    dependencies: [.package(url: "https://github.com/Hi-Rez/Satin", from: "1.6.1")],
     targets: [.target(name: "Youi", dependencies: ["Satin"], resources: [.process("Resources")])],
     swiftLanguageVersions: [.v5]
 )
